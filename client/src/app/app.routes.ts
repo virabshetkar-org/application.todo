098b06bf9a3d27c193cdc1cbdfadd38966562032
Virab Shetkar <virabarunshetkar@gmail.com>
2026-04-15 09:35:37 +0530

    Updated todo client

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,11 @@
+import { provideHttpClient } from '@angular/common/http';
 import { Routes } from '@angular/router';
+import { ConfigurationService } from './shared/services/configuration.service';
 
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/todo/todo.page').then((c) => c.TodoPage),
+    providers: [provideHttpClient(), ConfigurationService],
   },
 ];
