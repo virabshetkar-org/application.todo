51ca1d630de8e69a3661e0a89dccb5a5e68c2c78
Virab Shetkar <virabarunshetkar@gmail.com>
2026-04-19 17:41:02 +0530

    Added DummyController

---
+++ server/Controllers/DummyController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Todo.Controllers;
+
+[ApiController]
+[Route("/api/todo/[controller]")]
+public class DummyController : ControllerBase
+{
+    [HttpGet(Name = "HelloWorld")]
+    public string Get()
+    {
+        return "Hello, World!";
+    }
+}
